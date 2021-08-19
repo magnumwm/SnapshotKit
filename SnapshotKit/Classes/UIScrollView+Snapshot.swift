a9c1e27ac17003bb986a1acba37002efa022a056
liukun <kun.liu@magnumwm.com>
2021-08-19 11:39:49 +0800

    [修改] 绘制scrollView部分内容

--- SnapshotKit/Classes/UIScrollView+Snapshot.swift
+++ SnapshotKit/Classes/UIScrollView+Snapshot.swift
@@ -76,16 +76,52 @@ extension UIScrollView {
         }
     }
 
-    fileprivate func drawScreenshotOfPageContent(_ index: Int, maxIndex: Int, completion: @escaping () -> Void) {
+    public func asyncTakeSnapshotOfPartialContent(_ contentRect:CGRect,
+                                                  composition: ((CGContext) -> CGFloat)? = nil,
+                                                  _ completion: @escaping ((UIImage?) -> Void)) {
+        // 分页绘制内容到ImageContext
+        let originalOffset = self.contentOffset
+
+        // 当contentSize.height<bounds.height时，保证至少有1页的内容绘制
+        var pageNum = 1
+        if contentRect.size.height > self.bounds.height {
+            pageNum = Int(floorf(Float(contentRect.size.height / self.bounds.height)))
+        }
+
+        let backgroundColor = self.backgroundColor ?? UIColor.white
+
+        UIGraphicsBeginImageContextWithOptions(contentRect.size, true, 0)
+
+        guard let context = UIGraphicsGetCurrentContext() else {
+            completion(nil)
+            return
+        }
+        context.setFillColor(backgroundColor.cgColor)
+        context.setStrokeColor(backgroundColor.cgColor)
+
+        var offset:CGFloat = 0
+        if let composition = composition {
+            offset = composition(context)
+        }
+
+        self.drawScreenshotOfPageContent(0, maxIndex: pageNum, offset: offset) {
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            self.contentOffset = originalOffset
+            completion(image)
+        }
+    }
+
+    fileprivate func drawScreenshotOfPageContent(_ index: Int, maxIndex: Int, offset: CGFloat = 0, completion: @escaping () -> Void) {
 
         self.setContentOffset(CGPoint(x: 0, y: CGFloat(index) * self.frame.size.height), animated: false)
-        let pageFrame = CGRect(x: 0, y: CGFloat(index) * self.frame.size.height, width: self.bounds.size.width, height: self.bounds.size.height)
+        let pageFrame = CGRect(x: 0, y: CGFloat(index) * self.frame.size.height + offset, width: self.bounds.size.width, height: self.bounds.size.height)
 
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.3) {
             self.drawHierarchy(in: pageFrame, afterScreenUpdates: true)
 
             if index < maxIndex {
-                self.drawScreenshotOfPageContent(index + 1, maxIndex: maxIndex, completion: completion)
+                self.drawScreenshotOfPageContent(index + 1, maxIndex: maxIndex, offset: offset, completion: completion)
             }else{
                 completion()
             }
